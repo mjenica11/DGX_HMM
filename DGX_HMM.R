17d39dfe0895edee33c2b6f57243fda59eb1b61f
mjenica11 <m.j.peters625@gmail.com>
2020-04-26 15:46:49 -0700

    replaced function to simulate data

--- DGX_HMM.R
+++ DGX_HMM.R
@@ -25,109 +25,49 @@ library(dplyr)
 #_____________________________________________________________________________________
 # Modify to simulate 3 states 
 #_____________________________________________________________________________________
-Simulate <- function(N, dice.val, count.avg, switch.val){
-    up.dice <- sample(1:dice.val, N, replace=T) 
-    down.dice <- sample(1:dice.val, N, replace=T)
-    nodiff.dice <- sample(1:dice.val, N, replace=T)
-    up.count.avg <- rpois(N, count.avg[1])
-    down.count.avg <- rpois(N, count.avg[2])
-    nodiff.count.avg <- rpois(N, count.avg[3])
-    # States
-    results <- data.frame(state=rep(NA,N), obs=rep(NA,N), dice=rep(NA,N))
-
-    results$state[1] <- "nodiff"
-    results$obs[1] <- nodiff.count.avg[1]
-    results$dice[1] <- nodiff.dice[1]
-
-    for (k in 2:N){
-        if(results$state[k-1]=="up"){
-             # if in the up state and rolled value > switch, stay in up
-            if(results$dice[k-1] > switch.val){
-                 results$state[k] <- "up"
-                 results$obs[k] <- up.count.avg[k]
-                 results$dice[k] <- up.dice[k]
-             # if < switch, move to down state
-             }else if(results$dice[k-1] < switch.val){
-                 results$state[k] <- "down"
-                 results$obs[k] <- down.count.avg[k]
-                 results$dice[k] <- down.dice[k]
-             # if = switch, move to no diff state
-             }else{
-                 results$state[k] <- "nodiff"
-                 results$obs[k] <- nodiff.count.avg[k]
-                 results$dice[k] <- nodiff.dice[k]
-             }
-         # if currently in down state
-         }else if(results$state[k-1]=="down"){
-              if(results$dice[k-1] > switch.val){
-                  results$state[k] <- "up"
-                  results$obs[k] <- up.count.avg[k]
-                  results$dice[k] <- up.dice[k]
-             # if < switch, move to down state
-             }else if(results$dice[k-1] < switch.val){
-                 results$state[k] <- "down"
-                 results$obs[k] <- down.count.avg[k]
-                 results$dice[k] <- down.dice[k]
-             # if = switch, move to no diff state
-             }else{
-                 results$state[k] <- "nodiff"
-                 results$obs[k] <- nodiff.count.avg[k]
-                 results$dice[k] <- nodiff.dice[k]
-             }
-         # if currently in nodiff state
-         }else {
-             if(results$dice[k-1] > switch.val){
-                  results$state[k] <- "up"
-                  results$obs[k] <- up.count.avg[k]
-                  results$dice[k] <- up.dice[k]
-             # if < switch, move to down state
-             }else if(results$dice[k-1] < switch.val){
-                 results$state[k] <- "down"
-                 results$obs[k] <- down.count.avg[k]
-                 results$dice[k] <- down.dice[k]
-             # if = switch, move to no diff state
-             }else{
-                 results$state[k] <- "nodiff"
-                 results$obs[k] <- nodiff.count.avg[k]
-                 results$dice[k] <- nodiff.dice[k]
-             }
-         }
-    }
-   return(cbind(gene=1:N, results))
-}
-
-# Simulate scenario
-set.seed(2)
-results <- Simulate(N=100, count.avg=c(100,1,50), dice.val=6, switch.val=3)
-head(results)
-
 # Observe results
-ggplot(results, aes(x=gene, y=obs)) + geom_line()
 
 
 #_____________________________________________________________________________________
 # Different way to simulate data 
 #_____________________________________________________________________________________
-state = 1
-observations<-0
-N <- 100
-pi_tran <- c(0.1, 0.3, 0,4)
-emission_parm <- matrix(c(1,5,10,5,15,5),ncol=2,byrow = T)
+# Simulate counts using a Gaussian distribution 
+Gaussian_Simulate <- function(Num_Samples, Transition, Emission){
+    state = 1
+    observations<-0
+    N <- Num_Samples
+    pi_tran <- Transition 
+    emission_parm <- matrix(Emission,ncol=2,byrow = T)
 
-for(i in 2:N){
-    uni<-runif(1)
-    if(runif(1)<pi_tran[1]){
-        state[i]<-1
-        observations[i]<-rnorm(n=1,mean = emission_parm[1,1],sd=emission_parm[1,2])
-    }else if(uni<pi_tran[2]){
-        state[i]<-2
-        observations[i]<-rnorm(n=1,mean = emission_parm[2,1],sd=emission_parm[2,2])
-    }else{
-    state[i]<-3
+    for(i in 2:N){
+        uni<-runif(1)
+        if(runif(1)<pi_tran[1]){
+            state[i]<-1
+            observations[i]<-rnorm(n=1,mean = emission_parm[1,1],sd=emission_parm[1,2])
+        }else if(uni<pi_tran[2]){
+            state[i]<-2
+            observations[i]<-rnorm(n=1,mean = emission_parm[2,1],sd=emission_parm[2,2])
+        }else{
+        state[i]<-3
         observations[i]<-rnorm(n=1,mean = emission_parm[3,1],sd=emission_parm[3,2])
+        }
     }
+
+    # Combine observations, states into df
+    results <- data.frame(state=rep(NA,N), obs=rep(NA,N))
+    results$obs <- observations
+    results$state <- state
+    return(cbind(gene=1:N, results))
 }
 
+
+# Simulate scenario
+set.seed(2)
+results <- Gaussian_Simulate(Num_Samples=100, Transition=c(0.1,0.3,0.4), Emission=c(1,5,10,5,15,5))
+head(results)
+
+# Observe results
+ggplot(results, aes(x=gene, y=obs)) + geom_line()
 
 #_____________________________________________________________________________________
 # Function to make an HMM with depmix 
